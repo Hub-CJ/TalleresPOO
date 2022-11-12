1ac20b1bb58b0f869cf8095b11b21c1c3bdb6f44
HubCJ <u20201b742@upc.edu.pe>
2022-11-12 10:16:19 -0500

    second commit

--- Semana13/ejercicio2.py
+++ Semana13/ejercicio2.py
@@ -4,11 +4,12 @@ import os
 os.system("cls")
 def calcularBalance(df, meses):
     df["balance"] = df["ventas"] - df["gastos"]
-    print(df)
+    #print( df.set_index("meses").loc[meses, "balance"].sum())
+    return df.set_index("meses").loc[meses, "balance"].sum()
 
 meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
 ventas = [1256, 1200, 1034, 1456, 1567, 1456, 1456, 1456, 1456, 1456, 1456, 1456]
 gastos = [1836, 158, 147, 198, 1657, 1457, 1457, 1457, 145, 13554, 1457, 1457]
 df = pd.DataFrame({"meses": meses, "ventas": ventas, "gastos": gastos})
 
-calcularBalance(df, ["enero", "febrero"])
+print(calcularBalance(df, ["enero", "diciembre"]))
